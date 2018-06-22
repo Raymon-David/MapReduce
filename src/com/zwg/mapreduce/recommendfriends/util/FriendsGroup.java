20a9c50fd136b75e1fab9d0d8c440bc655824e71
weiguo.zhang <xingyunruyubb@gmail.com>
2018-06-22 14:16:44 +0800

    推荐好友、排序，将推荐好友的权重显示

---
+++ src/com/zwg/mapreduce/recommendfriends/util/FriendsGroup.java
@@ -0,0 +1,18 @@
+package com.zwg.mapreduce.recommendfriends.util;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class FriendsGroup extends WritableComparator {
+
+    public FriendsGroup(){
+        super(User.class, true);
+    }
+
+    public int compare(WritableComparable a, WritableComparable b) {
+        User u1 = (User) a;
+        User u2 = (User) b;
+
+        return u1.getUname().compareTo(u2.getUname());
+    }
+}
